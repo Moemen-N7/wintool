8dfd790e54d98e1c2f95ee3fca8af36b20e47989
Mo'men Salem <40383259+Moemen-N7@users.noreply.github.com>
2023-03-10 17:23:28 +0200

    Update wintool.bat

--- wintool.bat
+++ wintool.bat
@@ -1,9 +1,5 @@
 @echo off
 color 4
-set g=‪D:\move\1
-set 2=D:\move\2
-set 3=‪D:\move\3
-set 4=‪D:\move\‪
 set pass=mmas2005
 set "params=%*"
 cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/c cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
