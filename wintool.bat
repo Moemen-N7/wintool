8e58268e9ca33af1998ebddffefb6aff2fe7bb61
Mo'men Salem <40383259+Moemen-N7@users.noreply.github.com>
2023-03-11 19:10:09 +0200

    Update wintool.bat

--- wintool.bat
+++ wintool.bat
@@ -1,6 +1,5 @@
 @echo off
 color 4
-set pass=mmas2005
 set "params=%*"
 cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/c cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
 :pass
@@ -12,19 +11,6 @@ echo ##################################################
 echo Thanks For Using My tool  :)
 echo ############################
 
-set /p password=Enter password:
-if "%password%"=="%pass%" (
-
-    goto menu
-) else (
-cls
-echo Invalid pass. Please try again.
-pause
-cls
-goto pass
-cls
-
-)
 
 :menu
 color 4
